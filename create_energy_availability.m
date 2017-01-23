82ee67cd7604f9a730671ef255429e60350a3538
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-01-23 16:25:08 +0200

    create energy availability matrix

---
+++ create_energy_availability.m
@@ -0,0 +1,14 @@
+function E = create_energy_availability(m)
+% USAGE
+%  E = create_energy_availability(m)
+%
+% INPUTS
+%  m          - number of devices
+%
+% OUTPUTS
+%  E        - [m x 1] Energy availability for every device
+
+E = rand(m,1);
+
+end
+
--- run_simulation.m
+++ run_simulation.m
@@ -4,16 +4,19 @@ clear;
 % PARAMETERS
 %  m          - number of devices
 %  a          - probability for device to form own cluster
-%  w_1        - weight for ID in cluster creation
-%  w_2        - weight for D in cluster creation
+%  wa_1       - weight for ID in cluster creation
+%  wa_2       - weight for D in cluster creation
+%  wb_1       - weight for ID in clusterhead selection
+%  wb_2       - weight for D in clusterhead selection
+%  wb_3       - weight for E in clusterhead selection
 
 m = 5;
 a = 2;
-w_1 = 0.5;
-w_2 = 0.5;
+wa_1 = 0.5;
+wa_2 = 0.5;
 
-if w_1 + w_2 ~= 1
-    error('w_1 and w_2 don''t sum to 1');
+if wa_1 + wa_2 ~= 1
+    error('wa_1 and wa_2 don''t sum to 1');
 end
 
 [E_i, E_d] = create_matrices(m);
@@ -21,4 +24,8 @@ end
 ID = -log2(E_i);
 D = -log2(E_d);
 
-clusters = create_clusters(ID, D, a, w_1, w_2);
+clusters = create_clusters(ID, D, a, wa_1, wa_2);
+
+E = create_energy_availability(m);
+
+% cluster_heads = find_clusterheads(ID, D, wb_1, wb_2, wb_3);
