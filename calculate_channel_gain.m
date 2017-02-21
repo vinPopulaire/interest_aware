26447e014e3d337652b7fefd3dd4a0f1556856cb
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-02-21 13:18:27 +0200

    split calculation of channel gain in a different function

---
+++ calculate_channel_gain.m
@@ -0,0 +1,32 @@
+function G = calculate_channel_gain(distances)
+% USAGE
+%  [powers, G] = find_powers_to_maximize_utility(cluster,clusterhead, distances)
+%
+% INPUTS
+%  clusters     - [1 x m] cells with[1 x y] array with ids of nodes in cluster
+%  clusterheads - [1 x m] ids of the clusterhead
+%  distances    - physical distance between nodes of cluster
+%
+% OUTPUTS
+%  G           - [y x y] matrix with Channel Gain of pairs
+
+num_all_devices = size(distances,2);
+
+% G   ->channel gain
+% a=2 -> indoor only communication
+G = zeros(num_all_devices,num_all_devices);
+a = 2;
+
+% Calculate channel gain for all devices
+% (not only those of the cluster) because all contribute to the
+% interference detected
+for ii = 1:num_all_devices
+    for jj = ii:num_all_devices
+%         G(ii,jj) = lognrnd(0,1.84)/distances(ii,jj);
+        G(ii,jj) = 0.097/distances(ii,jj)^a;
+        G(jj,ii) = G(ii,jj);
+    end
+    G(ii,ii) = 0;
+end
+
+end
--- find_powers_to_maximize_utility.m
+++ find_powers_to_maximize_utility.m
@@ -1,33 +1,16 @@
-function powers = find_powers_to_maximize_utility(clusters,clusterheads, distances)
+function powers = find_powers_to_maximize_utility(clusters,clusterheads, G)
 % USAGE
 %  powers = find_powers_to_maximize_utility(cluster,clusterhead, distances)
 %
 % INPUTS
 %  clusters     - [1 x m] cells with[1 x y] array with ids of nodes in cluster
 %  clusterheads - [1 x m] ids of the clusterhead
-%  distances    - physical distance between nodes of cluster
+%  G            - [y x y] matrix with Channel Gain of pairs
 %
 % OUTPUTS
 %  powers      - [1 x y] array with powers needed for transmission
 
-num_all_devices = size(distances,2);
-
-% G   ->channel gain
-% a=2 -> indoor only communication
-G = zeros(num_all_devices,num_all_devices);
-a = 2;
-
-% Calculate channel gain for all devices
-% (not only those of the cluster) because all contribute to the
-% interference detected
-for ii = 1:num_all_devices
-    for jj = ii:num_all_devices
-%         G(ii,jj) = lognrnd(0,1.84)/distances(ii,jj);
-        G(ii,jj) = 0.097/distances(ii,jj)^a;
-        G(jj,ii) = G(ii,jj);
-    end
-    G(ii,ii) = 0;
-end
+num_all_devices = size(G,2);
 
 % arbitrarily set all starting powers to 0.5
 current_powers = 0.5*ones(1, num_all_devices);
