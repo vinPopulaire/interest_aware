bfabb15a8768142a033085c94ba814104ca4ada9
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-03-17 14:27:58 +0200

    create function to plot results

---
+++ plot_results.m
@@ -0,0 +1,14 @@
+figure;
+hold on;
+
+plot(1:rounds,total_system_energy_consumed{1,1},'b','DisplayName','distance');
+plot(1:rounds,total_system_energy_consumed{2,2},'g','DisplayName','balanced');
+plot(1:rounds,total_system_energy_consumed{3,3},'r','DisplayName','interest');
+legend('show')
+
+figure;
+hold on;
+plot(1:rounds,total_power_info_transmission{1,1},'b','DisplayName','distance');
+plot(1:rounds,total_power_info_transmission{2,2},'g','DisplayName','balanced');
+plot(1:rounds,total_power_info_transmission{3,3},'r','DisplayName','interest');
+legend('show')
