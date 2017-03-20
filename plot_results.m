60c5fb24be0497d0bca3df911f7d7d2ab6ef7279
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-03-20 11:39:37 +0200

    add title to plots

--- plot_results.m
+++ plot_results.m
@@ -1,6 +1,7 @@
 figure;
 hold on;
 
+title('Total system energy consumed');
 plot(1:rounds,total_system_energy_consumed{1,1},'b','DisplayName','distance');
 plot(1:rounds,total_system_energy_consumed{2,2},'g','DisplayName','balanced');
 plot(1:rounds,total_system_energy_consumed{3,3},'r','DisplayName','interest');
@@ -8,6 +9,7 @@ legend('show')
 
 figure;
 hold on;
+title('Total power used for information transmission');
 plot(1:rounds,total_power_info_transmission{1,1},'b','DisplayName','distance');
 plot(1:rounds,total_power_info_transmission{2,2},'g','DisplayName','balanced');
 plot(1:rounds,total_power_info_transmission{3,3},'r','DisplayName','interest');
