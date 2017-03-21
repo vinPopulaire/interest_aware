f990bbc44c0bf9ddd7e6f19e7b3ce2344d1cbc85
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-03-21 15:30:00 +0200

    save plots in file

--- plot_results.m
+++ plot_results.m
@@ -1,13 +1,20 @@
+for repetition = 1:size(total_system_energy_consumed,1)
+
+% repetition = 1;
+
 figure;
 hold on;
-
-repetition = 1;
 
 title('Total system energy consumed');
 plot(1:rounds,total_system_energy_consumed{repetition}{1,1},'b','DisplayName','distance');
 plot(1:rounds,total_system_energy_consumed{repetition}{2,2},'g','DisplayName','balanced');
 plot(1:rounds,total_system_energy_consumed{repetition}{3,3},'r','DisplayName','interest');
 legend('show')
+
+printpdf(['figures/', num2str(repetition), 'a']);
+
+set(gcf, 'PaperPositionMode','auto')
+print('-dpng',['figures/energy', num2str(repetition), 'a'])
 
 figure;
 hold on;
@@ -16,3 +23,12 @@ plot(1:rounds,total_power_info_transmission{repetition}{1,1},'b','DisplayName','
 plot(1:rounds,total_power_info_transmission{repetition}{2,2},'g','DisplayName','balanced');
 plot(1:rounds,total_power_info_transmission{repetition}{3,3},'r','DisplayName','interest');
 legend('show')
+
+printpdf(['figures/', num2str(repetition), 'b']);
+
+set(gcf, 'PaperPositionMode','auto')
+print('-dpng',['figures/power', num2str(repetition), 'b'])
+
+close all
+
+end
